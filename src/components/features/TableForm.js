08757b9add178d8134ad37dac4ca1f7ea7ff1041
Magdalena Lesisz <meghara@icloud.com>
2023-10-28 21:24:09 +0200

    Table Form

--- src/components/features/TableForm.js
+++ src/components/features/TableForm.js
@@ -1,82 +1,92 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateTablesRequest } from '../../redux/tablesReducer';
 import { Container, Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 
-
 const TableForm = (props) => {
-  const id = props.tableId;
-    const dispatch = useDispatch();
-    const [status, setStatus] = useState(props.status);
-    const [peopleAmount, setPeople] = useState(props.peopleAmount);
-    const [maxPeopleAmount, setmaxPeople] = useState(props.maxPeopleAmount);
-    const [bill, setBill] = useState(props.bill);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      if (status !== 'Busy') {
-        setBill(0);
-      } else if (status === 'Reserved') {
-        setBill(0);
-        setStatus(status);
-      } else if (status === 'Free' || status === 'Cleaning') {
-        setPeople(0);
-      }
-    }, [status, bill]);
+  const [status, setStatus] = useState(props.status);
+  const [peopleAmount, setPeopleAmount] = useState(props.peopleAmount);
+  const [maxPeopleAmount, setMaxPeopleAmount] = useState(props.maxPeopleAmount);
+  const [bill, setBill] = useState(props.bill);
 
-    useEffect(() => {
-      if (maxPeopleAmount > 10) {
-        alert('Max People 10');
-        setmaxPeople(10);
-      } else if (peopleAmount > 10) {
-        setPeople(10);
-      } else if (maxPeopleAmount < 0) {
-        setmaxPeople(0);
-      } else if (peopleAmount < 0) {
-        setPeople(0);
-      } else if (peopleAmount > maxPeopleAmount) {
-        alert('WARNING!!! People cannot be higher than MaxPeople ');
-        setPeople(maxPeopleAmount);
-      }
-    }, [maxPeopleAmount, peopleAmount]);
-
-    const handleSubmit = event =>{
-      event.preventDefault();
-      dispatch(updateTablesRequest({id,status,peopleAmount,maxPeopleAmount,bill}))
+  const handleStatusChange = (newStatus) => {
+    setStatus(newStatus);
+    if (newStatus !== 'Busy') {
+      setBill(0);
     }
-    return (
-      <Container>
-          <Form>
-            <Form.Group>
-              <p>
-              Status:
-              <Form.Select key={id} aria-label="Select status" value={status} name="status" onChange={e => setStatus(e.target.value)}>
-                <option defaultValue={status}>{`${status}`}</option>
-                <option value="Busy">Busy</option>
-                <option value="Cleaning">Cleaning</option>
-                <option value="Reserved">Reserved</option>
-              </Form.Select>
-              </p>
-              <p>
-              People:
-              <Form.Control value={peopleAmount} onChange={e => setPeople(e.target.value)}></Form.Control> 
-              / 
-              <Form.Control value={maxPeopleAmount} onChange={e => setmaxPeople(e.target.value)}></Form.Control>
-              </p>
-              <p>
-              Bill ($): 
-              <Form.Control value={bill} onChange={e => setBill(e.target.value)}></Form.Control>
-              </p>
-              <p>
-              <Button onClick={handleSubmit}>SAVE</Button>
-              </p>
-            </Form.Group>
-          </Form>
-      </Container>
-      );
-  }
+    if (newStatus === 'Free' || newStatus === 'Cleaning') {
+      setPeopleAmount(0);
+    }
+  };
+
+  const handlePeopleAmountChange = (newPeopleAmount) => {
+    if (newPeopleAmount < 0) {
+      newPeopleAmount = 0;
+    }
+    if (newPeopleAmount > 10) {
+      newPeopleAmount = 10;
+    }
+    if (newPeopleAmount > maxPeopleAmount) {
+      newPeopleAmount = maxPeopleAmount;
+    }
+    setPeopleAmount(newPeopleAmount);
+  };
+
+  const handleMaxPeopleAmountChange = (newMaxPeopleAmount) => {
+    if (newMaxPeopleAmount < 0) {
+      newMaxPeopleAmount = 0;
+    }
+    if (newMaxPeopleAmount > 10) {
+      newMaxPeopleAmount = 10;
+    }
+    if (peopleAmount > newMaxPeopleAmount) {
+      setPeopleAmount(newMaxPeopleAmount);
+    }
+    setMaxPeopleAmount(newMaxPeopleAmount);
+  };
 
 
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(updateTablesRequest({ ...props, status, peopleAmount, maxPeopleAmount, bill }));
+  };
+
+  return (
+    <Container>
+      <Form>
+        <Form.Group>
+          <p>
+            Status:
+            <Form.Select value={status} name="status" onChange={(e) => handleStatusChange(e.target.value)}>
+              <option value="Free">Free</option>
+              <option value="Busy">Busy</option>
+              <option value="Cleaning">Cleaning</option>
+              <option value="Reserved">Reserved</option>
+            </Form.Select>
+          </p>
+          <p>
+            People:
+            <Form.Control type="number" value={peopleAmount} onChange={(e) => handlePeopleAmountChange(parseInt(e.target.value))} />
+            /
+            <Form.Control type="number" value={maxPeopleAmount} onChange={(e) => handleMaxPeopleAmountChange(parseInt(e.target.value))} />
+          </p>
+          {status === 'Busy' && (
+            <p>
+              Bill ($):
+              <Form.Control type="number" value={bill} onChange={(e) => setBill(parseInt(e.target.value))} />
+            </p>
+          )}
+          <p>
+            <Button onClick={handleSubmit}>Update</Button>
+          </p>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+};
 
 export default TableForm;
